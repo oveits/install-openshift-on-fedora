bb118f0a4967bdc6309db6cd17e0c0789fa16687
Oliver Veits <oliver.veits@gmail.com>
2018-09-24 23:05:39 +0200

    corrected enable selinux script

--- 6_enable_selinux.sh
+++ 6_enable_selinux.sh
@@ -1,6 +1,6 @@
 
-if grep disabled /etc/selinux/config; then 
-   sed -i 's/SELINUX=(.*)/SELINUX=permissive/g' /etc/selinux/config 
+if grep 'SELINUX=disabled' /etc/selinux/config; then
+   sed -i 's/SELINUX=.*$/SELINUX=permissive/g' /etc/selinux/config
    reboot
 else
    echo 'Selinux is enabled; nothing to do'
