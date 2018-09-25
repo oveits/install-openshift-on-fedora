a8db55c54b2112298bfe039b08c005b67c29e8aa
Oliver Veits <oliver.veits@gmail.com>
2018-09-25 22:29:36 +0200

    activate the main script

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 for file in [1-9]_* ; do
-  echo bash $file || SUCCESS=false
+  bash $file || SUCCESS=false
   [ "$SUCCESS" == "false" ] && echo "script $file returned an error; stopping" && exit 1
 done
