1e82e574a642510226f128217814dcf837108dc5
Oliver Veits <oliver.veits@gmail.com>
2018-09-25 22:32:14 +0200

    corrected glob to avoid recursive call of the main shell script

--- 1_install_all.sh
+++ 1_install_all.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-for file in [1-9]_* ; do
+for file in [2-9]_* ; do
   bash $file || SUCCESS=false
   [ "$SUCCESS" == "false" ] && echo "script $file returned an error; stopping" && exit 1
 done
